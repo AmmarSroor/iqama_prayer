fd365374a64ec31d0c870dc5cc4f7583a893ec8d
aabusroor <ammarabusroor@gmail.com>
2021-10-26 13:54:54 +0300

    Iqama prayer

--- lib/features/dua_and_dhikr/data/datasources/dua_and_dhikr_local_data_source.dart
+++ lib/features/dua_and_dhikr/data/datasources/dua_and_dhikr_local_data_source.dart
@@ -1,3 +1,3 @@
-class DuaAndDhikrLocalDataSource{
+abstract class DuaAndDhikrLocalDataSource{
 
 }
--- lib/features/sub_dua_or_dhikr/data/datasources/sub_dua_or_dhikr_local_data_source.dart
+++ lib/features/sub_dua_or_dhikr/data/datasources/sub_dua_or_dhikr_local_data_source.dart
@@ -1,3 +1,3 @@
-class SubDuaOrDhikrLocalDataSource{
+abstract class SubDuaOrDhikrLocalDataSource{
 
 }
